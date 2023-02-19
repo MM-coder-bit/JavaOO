542d410abaf76e5a45cab5fe5a201716a5e89a36
Mateus <marques18@outlook.com>
2023-02-19 13:41:17 -0300

    criado segunda instanco da classe conta() e concatenado string com resultados

--- bytebank/src/cria_conta.java
+++ bytebank/src/cria_conta.java
@@ -1,8 +1,17 @@
 public class cria_conta {
     public static void main(String[] args) 
     {
-        conta PrimeiraConta = new conta();       // Intancia do Objeto conta()
-        PrimeiraConta.saldo = 200;               // Atribuição de valor no atributo saldo
-        System.out.println(PrimeiraConta.saldo); // print do saldo da intancia do objeto 'Primeira Conta'
+
+        conta PrimeiraConta = new conta(); // Intancia do Objeto conta() pelo comando new conta() e salva na memoria 'conta PrimeiraConta'
+        PrimeiraConta.saldo = 200;         // Atribuição de valor no atributo saldo
+
+        PrimeiraConta.saldo += 100;        // Somando 100 na instância Saldo da primeira conta
+        
+        conta SegundaConta = new conta();  // Intancia do Objeto conta() pelo comando new conta() e salva na memoria 'conta SegundaConta'
+        SegundaConta.saldo = 50;
+
+        System.out.println("primeira conta tem " + PrimeiraConta.saldo); // print do saldo da intancia do objeto 'Primeira Conta'
+        System.out.println("segunda conta tem " + SegundaConta.saldo);   // print do saldo da intancia do objeto 'Segunda Conta'
+
     }
 }
