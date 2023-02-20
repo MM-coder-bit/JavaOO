fbd76c41a2f20f28f18cfd79cb3fe491e167aeef
Mateus <marques18@outlook.com>
2023-02-20 11:49:51 -0300

    criado get e set da chamada de classe cliente() dentro da chamada conta()

--- bytebank/src/cliente.java
+++ bytebank/src/cliente.java
@@ -1,6 +1,39 @@
 public class cliente 
 {
-    String nome;
-    String cpf;
-    String profissao;
+    private String nome;
+    private String cpf;
+    private String profissao;
+
+    // --------- Get e Set do nome --------- //
+    public String getNome() 
+    {
+        return nome;
+    }
+
+    public void setNome(String nome) 
+    {
+        this.nome = nome;
+    }
+
+    // --------- Get e Set do CPF --------- //
+    public String getCpf() 
+    {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) 
+    {
+        this.cpf = cpf;
+    }
+
+    // --------- Get e Set da profissão --------- //
+    public String getProfissao() 
+    {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) 
+    {
+        this.profissao = profissao;
+    }
 }
--- bytebank/src/testeGetESet.java
+++ bytebank/src/testeGetESet.java
@@ -2,10 +2,18 @@ public class testeGetESet
 {
     public static void main(String[] args) 
     {
+        // cria conta
         conta contaTeste = new conta();
         contaTeste.setNumero(137);
-
         System.out.println(contaTeste.getNumero());
-
+        // cria cliente
+        cliente cliente1 = new cliente();
+        cliente1.setNome("Mateus Marques");
+        // Associa as classes
+        contaTeste.setTitular(cliente1);
+        System.out.println(contaTeste.getTitular().getNome());
+        // seta var por chamadas de classes
+        contaTeste.getTitular().setProfissao("Programador");
+        System.out.println(contaTeste.getTitular().getProfissao());
     }
 }
